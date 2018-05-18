f1d2aba65901fbcdd7e2c7b3e8a490f255905179
ArjunB-Hexaware <37321509+ArjunB-Hexaware@users.noreply.github.com>
2018-05-18 10:41:14 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -22,7 +22,51 @@ router.post('/botHandler',function(req, res){
 	let referer = req.get('Referer');
 	console.log('referer');
 	console.log(referer);
-	res.sendFile('login.html', {root: __dirname});
+	//res.sendFile('login.html', {root: __dirname});
+	res.send({
+    "conversationToken": "",
+    "expectUserResponse": true,
+    "expectedInputs": [
+        {
+            "inputPrompt": {
+                "richInitialPrompt": {
+                    "items": [
+                        {
+                            "simpleResponse": {
+                                "textToSpeech": "Math and prime numbers it is!"
+                            }
+                        },
+                        {
+                            "basicCard": {
+                                "title": "Math & prime numbers",
+                                "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                                "image": {
+                                    "url": "https://example.google.com/42.png",
+                                    "accessibilityText": "Image alternate text"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Read more",
+                                        "openUrlAction": {
+                                            "url": "https://example.google.com/mathandprimes"
+                                        }
+                                    }
+                                ],
+                                "imageDisplayOptions": "CROPPED"
+                            }
+                        }
+                    ],
+                    "suggestions": []
+                }
+            },
+            "possibleIntents": [
+                {
+                    "intent": "actions.intent.TEXT"
+                }
+            ]
+        }
+    ]
+});
 	var len = req.body.inputs.length;
 	for(i=0; i<len; i++){		
 		console.log(req.body.inputs[i].intent);
